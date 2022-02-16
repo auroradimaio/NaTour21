3045f9bfa09190200a883b630e183c32a16ec712
Mike Fonseta <seta.mik@gmail.com>
2022-02-16 11:58:57 +0100

    Aggiornamenti finali.

--- app/src/test/java/com/example/natour21/ImagePickerTest.java
+++ app/src/test/java/com/example/natour21/ImagePickerTest.java
@@ -1,8 +1,7 @@
 package com.example.natour21;
 
 import com.example.natour21.Utils.ImagePicker;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ImagePickerTest {
